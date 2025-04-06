a3600f8aefb186729f44eff6e7487f437dbe33b1
SigmaZardan <bibekbhujel077@gmail.com>
2025-04-06 07:30:06 +0545

    Initial Commit

---
+++ MeroLipi/MeroLipiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeroLipiApp.swift
+//  MeroLipi
+//
+//  Created by Bibek Bhujel on 06/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MeroLipiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeroLipiTests/MeroLipiTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeroLipiTests.swift
+//  MeroLipiTests
+//
+//  Created by Bibek Bhujel on 06/04/2025.
+//
+
+import Testing
+@testable import MeroLipi
+
+struct MeroLipiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
