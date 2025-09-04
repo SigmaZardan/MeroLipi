07b483f2ed943b3b4baa695ff663d25138864b62
SigmaZardan <bibekbhujel077@gmail.com>
2025-09-04 11:09:23 +0545

    OnboardingScreen feature has been added.

---
+++ MeroLipi/FirstStepInstructionView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstStepInstructionView.swift
+//  MeroLipi
+//
+//  Created by Bibek Bhujel on 03/09/2025.
+//
+
+import SwiftUI
+
+struct FirstStepInstructionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstStepInstructionView()
+}
---
+++ MeroLipi/SecondStepInstructionView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondStepInstructionView.swift
+//  MeroLipi
+//
+//  Created by Bibek Bhujel on 03/09/2025.
+//
+
+import SwiftUI
+
+struct SecondStepInstructionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondStepInstructionView()
+}
